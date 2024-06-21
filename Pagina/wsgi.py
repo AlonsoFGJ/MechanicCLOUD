6e9d566cb3443642960abd67e0629066e54574fe
AlonsoFGJ <alo.gonzalezj@duocuc.cl>
2024-06-21 11:44:01 -0400

    UPDATE: wsgi

--- Pagina/wsgi.py
+++ Pagina/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pagina.settings')
+# Añadir el directorio actual al sys.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+# Ajustar el nombre del módulo de configuración de Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pagina.settings")
+
+# Configuración de Django
 application = get_wsgi_application()
-
-app = application
