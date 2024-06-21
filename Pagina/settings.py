e5c193385bdd9c6a0cad28b15627243773b5067d
AlonsoFGJ <alo.gonzalezj@duocuc.cl>
2024-06-21 11:58:34 -0400

    UPDATE: wsgi_vercel

--- Pagina/settings.py
+++ Pagina/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR /'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #python manage.py collectstatic --upload-unhashed-files
